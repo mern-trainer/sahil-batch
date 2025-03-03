63f2d5f3e0cc54b32e25d7899e9b4d2c5da86876
VISHNU M K <futuralabs.mern@gmail.com>
2025-03-03 11:50:27 +0530

    basic question and answer

---
+++ JavaScript/advanced.js
@@ -0,0 +1,20 @@
+// 1. JavaScript -> JavaScript is used to create interactive and dynamic web pages.
+// 2. Variable -> Container to store data values.
+// 3. Identifiers -> Name of the variable, functions, etc.
+// 4. functions -> block of code to perform a task.
+// 5. Data types -> Type of data stored in a variable.
+// 6. String -> Collection of characters.
+// 7. Array -> Collection of elements.
+// 8. keywords -> Reserved words in javaScript.
+// 9. DOM -> Programing interface for web documents to access the HTML elements, manipulate the HTML elements, change the style of the HTML elements and handle events.
+// 10. DOM Methods -> getElementById(), getElementsByClassName(), getElementsByTagName(), querySelector(), querySelectorAll().
+// 11. Event -> Event is an action that occurs on the web page.
+// 12. let -> Re-declaration is not allowed, Re-assignment is allowed.
+// 13. const -> Re-declaration is not allowed, Re-assignment is not allowed.
+// 14. var -> Re-declaration is allowed, Re-assignment is allowed.
+// 15. loop -> A loop is a sequence of instructions that is repeated until a certain condition is met.
+// 16. switch -> A switch statement is a conditional statement that allows you to execute different blocks of code based on different conditions.
+// 17. if-else -> An if-else statement is a conditional statement that allows you to execute different blocks of code based on different conditions.
+// 18. for -> A for loop is a loop that executes a block of code a certain number of times.
+// 19. while -> A while loop is a loop that executes a block of code as long as a certain condition is true.
+// 20. do-while -> A do-while loop is a loop that executes a block of code once, and then repeatedly executes the block of code as long as a certain condition is true.

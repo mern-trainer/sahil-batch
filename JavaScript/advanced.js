4d72b6eea6386decec473086dfb4331c358eea68
VISHNU M K <futuralabs.mern@gmail.com>
2025-03-03 13:27:40 +0530

    object, destructure, spread

--- JavaScript/advanced.js
+++ JavaScript/advanced.js
@@ -28,3 +28,82 @@
 //                 condition is true.
 
 // 5 times.
+
+// object, destructure, spread operator, rest operator
+
+// object -> collection of properties and methods.
+// Collection of keys and values.
+
+const obj = {
+    name: "John", // string
+    age: 23, // number
+    address: {
+        city: "New York", // string
+        state: "NY" // string
+    }, // object
+    hobbies: ["Reading", "Writing", "Coding"] // array
+}
+
+obj.sdflsdf = 2309
+
+// obj.age = 30
+// obj.hobbies.splice(1, 2)
+
+console.log(obj);
+
+// const name = obj.name
+// const age = obj.age
+// const address = obj.address
+// const hobbies = obj.hobbies
+
+// console.log(name, age, address, hobbies);
+
+// destructure
+
+const { name, age, address, hobbies } = obj;
+
+console.log(name, age, address, hobbies);
+
+const arr = [1, 2, 3, 4]
+
+// const f = arr[0]
+// const s = arr[1]
+// const t = arr[2]
+// const fr = arr[3]
+
+const [f, s, t, fr] = arr
+
+console.log(f, s, t, fr);
+
+// spread operator
+
+const array = [1, 2, 3]
+
+const copyArray = [...array] // pass by reference & pass by value
+
+copyArray.push(10)
+
+console.log(array, copyArray)
+
+const obj1 = {
+    age: 20,
+    name: "John"
+}
+
+const obj2 = {...obj1}
+
+obj2.age = 30
+
+console.log(obj1, obj2);
+
+
+
+const a1 = [1, 2, 3, 4, 5];
+const a2 = [6, 7, 8, 9, 10]
+
+const a3 = [...a1, ...a2]
+
+console.log(a3);
+
+
+

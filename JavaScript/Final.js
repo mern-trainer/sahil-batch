afbef2fd896ee240a497e54c8e84480ab07c3b0f
VISHNU M K <futuralabs.mern@gmail.com>
2025-03-21 12:29:20 +0530

    class and object

--- JavaScript/Final.js
+++ JavaScript/Final.js
@@ -20,10 +20,63 @@ const sum = (n) => {
     return n + sum(n - 1) 
 }
 
-console.log(sum(10));
+// console.log(sum(10));
 
 // sum(4) => 10
 // sum(3) => 6
 // sum(2) => 3
 // sum(1) => 1
 // sum(0) => 0
+
+class Person{
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+
+    get(key) {
+        const res = this[key]
+        if (res == undefined) {
+            return console.log("Invalid key");
+        }
+        return res
+    }
+}
+
+class Child extends Person{ // extends Person -> inheritance
+
+}
+
+const person = new Child("John", 23)
+
+console.log(person.get("name"));
+
+// encapsulation -> binding properties and methods together
+// abstraction -> hiding the implementation details
+// polymorphism -> same method name can have different implementations
+
+class Vehicle{
+    constructor(make, model, year) {
+        this.make = make
+        this.model = model
+        this.year = year
+    }
+
+    display() {
+        return this.make + " " + this.model + " " + this.year
+    }
+}
+
+class Toyota extends Vehicle{
+    display() {
+        return "Toyota"
+    }
+
+    parentDisplay() {
+        return super.display()
+    }
+}
+
+const veh = new Toyota("Toyota", "Camry", 2022)
+
+console.log(veh.parentDisplay());
